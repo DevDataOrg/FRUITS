9b8eb80332d1750d93f3715c1797c120e7ad2666
DevDataOrg <48391348+DevDataOrg@users.noreply.github.com>
2021-04-13 21:41:09 +0300

    Initial Commit

---
+++ FRUITS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FRUITS
+//
+//  Created by Artyom Romanchuk on 13.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FRUITS/FRUITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUITSApp.swift
+//  FRUITS
+//
+//  Created by Artyom Romanchuk on 13.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct FRUITSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
